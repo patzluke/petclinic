4f33f1160cb5af1c5ba5d772b92495f598dbbb07
Patrick Astrero <patzluke@gmail.com>
2022-11-23 15:52:20 +0800

    second pet clinic project commit

---
+++ src/main/java/com/springframework/petclinic/model/Person.java
@@ -0,0 +1,22 @@
+package com.springframework.petclinic.model;
+
+public class Person {
+    public String firstName;
+    public String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
